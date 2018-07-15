846126acfdfe1f7153bf075988c2575006093faa
Lucas Aquino <lucas.aquino@tse.jus.br>
2018-07-15 12:46:56 -0300

    ajuste portlets

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='pf.manuais',
-    version='1.0a1',
+    version='1.0b1',
     description="Pacote para Manuais PF",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
